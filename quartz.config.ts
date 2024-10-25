dd5573017ccff0eb730170e9eefae4354e836baf
zzzzzllllllaaaa <3559128958@qq.com>
2024-10-25 19:39:15 +0800

    1

--- quartz.config.ts
+++ quartz.config.ts
@@ -30,7 +30,7 @@ const config: QuartzConfig = {
       },
       colors: {
         lightMode: {
-          light: "#B5A642",
+          light: "#7FFFAA",
           lightgray: "#e5e5e5",
           gray: "#b8b8b8",
           darkgray: "#4e4e4e",
