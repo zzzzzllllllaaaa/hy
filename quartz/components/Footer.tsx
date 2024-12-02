55b35fac6fe7407e4f30f159dbb0a35004a2e1cc
zzzzzllllllaaaa <3559128958@qq.com>
2024-12-02 19:33:25 +0800

    1

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -21,7 +21,7 @@ export default ((opts?: Options) => {
       <footer class={`${displayClass ?? ""}`}>
         <p>
           {i18n(cfg.locale).components.footer.createdWith}{" "}
-          <a href="https://quartz.jzhao.xyz/">Quartz v4.2.3 v{version}</a> © {year}
+          <a href="https://quartz.jzhao.xyz/">Quartz v{version}</a> © {year}
         </p>
         <ul class="footer-links">
           {links.map((link, index) => (
